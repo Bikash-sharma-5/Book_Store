4ba38a8894d56275d421667972265706394b6529
BIKASH KUMAR SHARMA <bikashsharma5151@gmail.com>
2025-03-22 04:10:48 +0530

    Update settings.py

--- bookstore/bookstore/bookstore/settings.py
+++ bookstore/bookstore/bookstore/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ulaa=9zb#cpsspryimnbt)(l2fko807(1w9avxu!x&$b5gt$z!'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
